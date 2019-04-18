d6f63ae53bb14e59d89f47a23b72b68d465dc3b2
李慧 <lihui37@meituan.com>
2019-04-18 22:45:27 +0800

    八皇后问题, 有bug, 待解决

---
+++ algo/src/main/java/flanker/recursive/EightQueen.java
@@ -0,0 +1,127 @@
+package flanker.recursive;
+
+public class EightQueen {
+
+    private static final int maxlenth = 8;
+    private int[][] flags;
+    private int flagsCount = 0;
+
+    public EightQueen() {
+        flags = new int[maxlenth][maxlenth];
+    }
+
+    public static boolean isValid(EightQueen eightQueen, int x, int y) {
+        return iSRowValid(eightQueen, x, y)
+                && isColumnValid(eightQueen, x, y)
+                &&
+                isDiagonalValid(eightQueen, x, y);
+
+    }
+
+    public static void findall() {
+        EightQueen eightQueen = new EightQueen();
+
+
+    }
+
+
+    public static void findInLine(EightQueen eightQueen, int y) {
+        for (int i = 0; i < maxlenth; i++) {
+            if (isValid(eightQueen, i, y)) {
+                putFlag(eightQueen, i, y);
+                eightQueen.flagsCount++;
+                System.out.println(String.format("put at %d %d, count %d", i, y, eightQueen.flagsCount));
+
+
+                if (y < maxlenth) {
+                    findInLine(eightQueen, y + 1);
+                } else {
+                    return;
+                }
+
+            }
+        }
+    }
+
+    public static EightQueen findOne() {
+
+        EightQueen eightQueen = new EightQueen();
+        int flags = 0;
+        while (true) {
+            int x = 0;
+            int y = 0;
+            if (isValid(eightQueen, x, y)) {
+                putFlag(eightQueen, x, y);//放下棋子,并且跳转到下一行
+                flags++;
+                if (flags == 8) {
+                    break;
+                }
+                x = 0;
+                y++;
+            } else {
+
+                if (x < maxlenth) {
+                    x++;//当前行内寻找
+                } else {
+                    //下一行寻找;
+                    y++;
+                    x = 0;
+                }
+                if (y <= maxlenth) {
+                    x--;
+                }
+            }
+        }
+        return eightQueen;
+    }
+
+    public static void print(EightQueen eightQueen) {
+        for (int i = 0; i < maxlenth; i++) {
+            for (int j = 0; j < maxlenth; j++) {
+                System.out.print(eightQueen.flags[i][j] == 0 ? "Q" : "X");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void putFlag(EightQueen eightQueen, int x, int y) {
+        eightQueen.flags[x][y] = 1;
+    }
+
+    public static boolean iSRowValid(EightQueen eightQueen, int x, int y) {
+
+        for (int i = 0; i < maxlenth; i++) {
+            if (eightQueen.flags[y][i] == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isColumnValid(EightQueen eightQueen, int x, int y) {
+        for (int i = 0; i < maxlenth; i++) {
+            if (eightQueen.flags[i][x] == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isDiagonalValid(EightQueen eightQueen, int x, int y) {
+        //左上到右下;
+        for (int i = x, j = y; i >= 0 && j >= 0; i--, j--) {
+            if (eightQueen.flags[i][j] == 1) {
+                return false;
+            }
+        }
+
+        //左下到右上;
+        for (int i = x, j = y; j >= 0 && i < maxlenth; i++, j--) {
+            if (eightQueen.flags[i][j] == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ algo/src/test/java/flanker/recursive/EightQueenTest.java
@@ -0,0 +1,45 @@
+package flanker.recursive;
+
+import org.junit.Test;
+
+public class EightQueenTest {
+
+    @Test
+    public void findOne() {
+
+        EightQueen eightQueen = EightQueen.findOne();
+        EightQueen.print(eightQueen);
+    }
+
+    @Test
+    public void findByLine() {
+        EightQueen eightQueen = new EightQueen();
+        EightQueen.findInLine(eightQueen, 0);
+        EightQueen.print(eightQueen);
+    }
+
+    @Test
+    public void isValid() {
+        EightQueen eightQueen = new EightQueen();
+        EightQueen.putFlag(eightQueen, 0, 0);
+        EightQueen.print(eightQueen);
+        System.out.println(EightQueen.isValid(eightQueen, 1, 0));
+        System.out.println(EightQueen.isValid(eightQueen, 1, 1));
+        System.out.println(EightQueen.isValid(eightQueen, 0, 1));
+
+
+        System.out.println(EightQueen.isValid(eightQueen, 2, 1));
+        System.out.println(EightQueen.isValid(eightQueen, 3, 1));
+        System.out.println(EightQueen.isValid(eightQueen, 2, 1));
+    }
+
+    @Test
+    public void isValid2() {
+        EightQueen eightQueen = new EightQueen();
+        EightQueen.putFlag(eightQueen, 1, 1);
+        EightQueen.print(eightQueen);
+        System.out.println(EightQueen.isValid(eightQueen, 1, 0));
+        System.out.println(EightQueen.isValid(eightQueen, 1, 1));
+        System.out.println(EightQueen.isValid(eightQueen, 0, 1));
+    }
+}
