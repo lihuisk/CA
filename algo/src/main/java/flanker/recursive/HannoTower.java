3011d4f52fe4a24fe7a8657f06afb07edf0b84c1
李慧 <lihui37@meituan.com>
2019-04-18 23:19:55 +0800

    汉诺塔

---
+++ algo/src/main/java/flanker/recursive/HannoTower.java
@@ -0,0 +1,17 @@
+package flanker.recursive;
+
+enum Position {
+    L,
+    M,
+    R,
+}
+
+public class HannoTower {
+
+    public static void move(int length, Position f, Position t) {
+
+        if (length <= 2) {
+
+        }
+    }
+}
